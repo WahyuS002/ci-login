9637934333444c4093e9ddbbde7797e0a7d37a5e
WahyuS002 <syahputrawahyu61@gmail.com>
2019-11-11 10:00:05 +0700

    #4 Login - Menambahkan method (login,logout) pada controller auth, menambahkan sebuah controller yaitu user.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -15,9 +15,10 @@
                                     <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
                                 </div>
                                 <?= $this->session->flashdata('message'); ?>
-                                <form class="user">
+                                <form class="user" method="post" action="<?= base_url(); ?>auth">
                                     <div class="form-group">
                                         <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address...">
+                                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>') ?>
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
